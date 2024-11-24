b78a5cde492d85e1a0d1301ceac527a19b628362
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-24 08:32:28 +0530

    GYM WEBSITE

---
+++ src/components/Contact.jsx
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { ClipLoader } from 'react-spinners';
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const sendMail = async(e)=>{
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const {data} = await axios.post("http://localhost:4000/send/mail", {name,email,message},
+        {
+          withCredentials: true,
+          headers: {"Content Type": "application/json"},
+        }
+      );
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success(data.message);
+      setLoading(false);
+      catch (error){
+        setLoading(false);
+      toast.error(error.response.data.message);
+      }
+    };
+  }
+  return (
+    <section className='contact'>
+      <form onSubmit={sendMail}>
+        <h1>Contact us</h1>
+        <div>
+          <label>Name</label>
+          <input type="text"
+           value={name}
+           onChange={(e) => setName(e.target.value)}
+           placeholder="Enter your name"
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email"
+           value={email}
+           onChange={(e) => setEmail(e.target.value)}
+           placeholder="Enter your email"
+          />
+        </div>
+        <div>
+          <label>Message</label>
+          <input type="text" value={message} 
+            onChange={(e) =>setMessage(e.target.value)}
+            placeholder="Type Your Message"
+            />
+        </div>
+        <button type="submit" disabled={loading} style={{display: "flex", justifyContent: "center" , alignItems:"center", gap: "15px"}}>
+          {loading && <ClipLoader size={20} color="white"/>}
+          Send Message
+        </button>
+      </form>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,36 @@
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos distinctio molestiae necessitatibus vero ullam, esse voluptas repellendus sequi?</p>
+      <img src="/public/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+      <h1>FEATURED BOOTCAMPS</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae rerum repellat asperiores incidunt maiores sequi eveniet blanditiis. Ipsam!</p>
+      <div className="bootcamps"> 
+         <div>
+          <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate rem, sunt aliquam error sapiente fugit!</p>
+         </div>
+         <div>
+          <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate rem, sunt aliquam error sapiente fugit!</p>
+         </div>
+         <div>
+          <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate rem, sunt aliquam error sapiente fugit!</p>
+         </div>
+         <div>
+          <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h4>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate rem, sunt aliquam error sapiente fugit!</p>
+         </div>
+      </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default WorkoutSessions
